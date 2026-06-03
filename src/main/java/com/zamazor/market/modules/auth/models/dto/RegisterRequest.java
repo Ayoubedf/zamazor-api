65c7b24dc6d23f684aae4188b4da0afdb49d255a
ayoub <ayoubelouadifi2005@gmail.com>
2026-06-03 18:50:52 +0000

    add RegisterRequest DTO

---
+++ src/main/java/com/zamazor/market/modules/auth/models/dto/RegisterRequest.java
@@ -0,0 +1,39 @@
+package com.zamazor.market.modules.auth.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Getter
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&.]).{8,}$",
+            message = "Password must be at least 8 characters long and include letters, numbers, and a special character."
+    )
+    private String password;
+
+    @NotBlank
+    @Size(min = 2)
+    private String name;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    @JsonIgnore
+    @AssertTrue(message = "You must be 18 years or older to register.")
+    public boolean isAdult() {
+        if (birthDate == null) return true;
+        return Period.between(birthDate, LocalDate.now()).getYears() >= 18;
+    }
+}
