cf7c2a3d86588f89b4784390ce20e9a3248401d9
ayoub <ayoubelouadifi2005@gmail.com>
2026-06-03 18:53:19 +0000

    add GET /users/{id} endpoint

---
+++ src/main/java/com/zamazor/market/modules/user/service/UserService.java
@@ -0,0 +1,23 @@
+package com.zamazor.market.modules.user.service;
+
+import com.zamazor.market.modules.user.models.dto.UserDto;
+import com.zamazor.market.modules.user.models.mapper.UserMapper;
+import com.zamazor.market.modules.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDto getUser(UUID id) {
+        return userRepository.findById(id).map(userMapper::toDto)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
