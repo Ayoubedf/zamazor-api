4787518a62713cbf22c6e01763fa113889ed98f5
ayoub <ayoubelouadifi2005@gmail.com>
2026-06-03 16:57:16 +0000

    add base SecurityConfiguration class

---
+++ src/main/java/com/zamazor/market/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.zamazor.market.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+}
