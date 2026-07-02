6a120a151fd4ade002b60b71f219abb52760275c
ayoub <ayoubelouadifi2005@gmail.com>
2026-07-02 23:42:03 +0000

    Fix LocalDateTime with Instant comparison

--- src/main/java/com/zamazor/market/modules/catalog/models/entity/Order.java
+++ src/main/java/com/zamazor/market/modules/catalog/models/entity/Order.java
@@ -16,6 +16,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -95,7 +96,7 @@ public class Order {
 		if (this.status == OrderStatus.DELIVERED || this.status == OrderStatus.REFUNDED) {
 			throw new OrderCancellationException("Cannot cancel an order that is already delivered or refunded.");
 		}
-		if (this.createdAt.isBefore(Instant.from(now.minusDays(2)))) {
+		if (this.createdAt.isBefore(Instant.from(now.minusDays(2).atZone(ZoneOffset.UTC).toInstant()))) {
 			throw new OrderCancellationException("Order cancellation window (2 days) has expired.");
 		}
 
@@ -110,7 +111,7 @@ public class Order {
 		if (this.status != OrderStatus.DELIVERED) {
 			throw new OrderRefundException("Only completed or delivered orders can be refunded.");
 		}
-		if (this.createdAt.isBefore(Instant.from(now.minusDays(2)))) {
+		if (this.createdAt.isBefore(Instant.from(now.minusDays(2).atZone(ZoneOffset.UTC).toInstant()))) {
 			throw new OrderRefundException("Order exceeds the 30-day refund policy window.");
 		}
 
